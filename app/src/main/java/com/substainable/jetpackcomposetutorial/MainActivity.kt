e04ebd7be2f053c0096470fab2351cd3668df73d
Joona Säkkinen <joonasakkinen@gmail.com>
2025-01-18 08:16:53 +0200

    Initial commit

---
+++ app/src/main/java/com/substainable/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,208 @@
+package com.substainable.jetpackcomposetutorial
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.border
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.substainable.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTutorialTheme {
+                Conversation(SampleData.conversationSample)
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row (modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.kallio),
+            contentDescription = "some photo",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetpackComposeTutorialTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+
+@Composable
+fun PreviewMessageCard() {
+    JetpackComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = Message("joku", "tekstiä")
+            )
+        }
+    }
+}
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Jokunen",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Jokunen",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        Message(
+            "Jokunen",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        Message(
+            "Jokunen",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Jokunen",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        Message(
+            "Jokunen",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Jokunen",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Jokunen",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Jokunen",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Jokunen",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Jokunen",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Jokunen",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Jokunen",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
